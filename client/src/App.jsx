0432ad618007ff74b51229f42450e9d5b719df00
liambrem <liam.e.brem@gmail.com>
2025-06-20 16:48:52 -0700

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './components/header-footer/Navbar'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () =>{
+		return (
+			<div className="App">
+        <Navbar /> 
+			</div>
+		)
+	}
 
-  return (
-    <>
-    </>
-  )
-}
-
-export default App
+export default App;
