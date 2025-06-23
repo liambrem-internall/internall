cf95703cd1ada4caffd0278de21696138d6a6b33
Liam Edward Brem <liambrem@meta.com>
2025-06-23 16:44:06 -0700

    Included bootstrap in navbar + sections

--- client/src/components/main-content/SectionList.jsx
+++ client/src/components/main-content/SectionList.jsx
@@ -3,20 +3,22 @@ import Item from "./Items/Item";
 import "./SectionList.css";
 import { useState } from "react";
 import { DndContext } from "@dnd-kit/core";
+import Container from "react-bootstrap/Container";
+
 
 const SectionList = () => {
   const [isDropped, setIsDropped] = useState(false);
   const draggableMarkup = <Item />;
 
   return (
-    <div className="section-list-container">
-      <div className="section-list">
+    <Container className="section-list-container">
+      <div className="row justify-content-center">
         <DndContext onDragEnd={handleDragEnd}>
           {!isDropped ? draggableMarkup : null}
           <Section>{isDropped ? draggableMarkup : "Drop here"}</Section>
         </DndContext>
       </div>
-    </div>
+    </Container>
   );
   function handleDragEnd(event) {
     if (event.over && event.over.id === "droppable") {
