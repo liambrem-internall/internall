ead0e302fa762c381b347075e592161f6b177779
liambrem <liam.e.brem@gmail.com>
2025-06-23 11:41:53 -0700

    basic drag & drop functionality

--- client/src/components/main-content/SectionList.jsx
+++ client/src/components/main-content/SectionList.jsx
@@ -1,14 +1,28 @@
 import Section from "./Sections/Section";
-import "./SectionList.css"
+import Item from "./Items/Item";
+import "./SectionList.css";
+import { useState } from "react";
+import { DndContext } from "@dnd-kit/core";
 
 const SectionList = () => {
+  const [isDropped, setIsDropped] = useState(false);
+  const draggableMarkup = <Item />;
+
   return (
     <div className="section-list-container">
       <div className="section-list">
-        <Section />
+        <DndContext onDragEnd={handleDragEnd}>
+          {!isDropped ? draggableMarkup : null}
+          <Section>{isDropped ? draggableMarkup : "Drop here"}</Section>
+        </DndContext>
       </div>
     </div>
   );
+  function handleDragEnd(event) {
+    if (event.over && event.over.id === "droppable") {
+      setIsDropped(true);
+    }
+  }
 };
 
 export default SectionList;
