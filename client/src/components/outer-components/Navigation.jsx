60d803451aabfe5f7da338861e74fbc96f464002
liambrem <liam.e.brem@gmail.com>
2025-06-26 13:46:25 -0700

    switch between board/list view - navbar update

--- client/src/components/outer-components/Navigation.jsx
+++ client/src/components/outer-components/Navigation.jsx
@@ -2,9 +2,21 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
+import { ViewModes } from "../../utils/constants";
 import "./Navbar.css";
 
-const Navigation = () => {
+
+const selectedStyle = {
+  color: "var(--pink1)",
+  fontWeight: "bold",
+  textDecoration: "underline",
+};
+
+const notSelectedStyle = {
+  color: "#fff",
+};
+
+const Navigation = ({ clickBoard, clickList, mode }) => {
   return (
     <div className="navbar-float-wrapper">
       <Navbar expand="lg" className="custom-navbar px-4 py-2">
@@ -16,8 +28,20 @@ const Navigation = () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <div className="d-flex ms-auto align-items-center">
               <Nav className="mx-auto">
-                <Nav.Link className="nav-link-custom">Board</Nav.Link>
-                <Nav.Link className="nav-link-custom">List</Nav.Link>
+                <Nav.Link
+                  className="nav-link-custom"
+                  style={mode === ViewModes.BOARD ? selectedStyle : notSelectedStyle}
+                  onClick={clickBoard}
+                >
+                  Board
+                </Nav.Link>
+                <Nav.Link
+                  className="nav-link-custom"
+                  style={mode === ViewModes.LIST ? selectedStyle : notSelectedStyle}
+                  onClick={clickList}
+                >
+                  List
+                </Nav.Link>
               </Nav>
               <Button className="get-started-btn ms-3">Account</Button>
             </div>
