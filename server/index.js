e139b6bbd04f9def90580149a77845ea5d4c8e24
liambrem <liam.e.brem@gmail.com>
2025-06-27 14:27:51 -0700

    successful connection to mongo

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+require('dotenv').config();
+
 const app = express()
 const PORT = 3000
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
+
+const connectDB = require('./utils/db');
+connectDB();
