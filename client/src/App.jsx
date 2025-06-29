614f961b233733a41394dc1625d0ea76cb49a7b2
Liam Edward Brem <liambrem@meta.com>
2025-06-29 15:26:33 -0700

    logged in/out redirects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
 import Navigation from "./components/outer-components/Navigation";
 import SectionList from "./components/main-content/SectionList";
 import { ViewModes } from "./utils/constants";
 import ViewContext from "./ViewContext";
-//import { Auth0Provider } from "@auth0/auth0-react";
-import { Auth0Provider } from "@auth0/auth0-react/dist/auth0-react.esm.js";
+import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
 import LoggedOut from "./components/logged-out-page/LoggedOut";
 
 import LightBallsOverlay from "./components/visuals/LightBallsOverlay";
 import "./App.css";
 
-
-
 const App = () => {
   const [viewMode, setViewMode] = useState(ViewModes.BOARD);
-  const domain = import.meta.env.VITE_AUTH0_DOMAIN
-  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
-  
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+
+  const HomeRedirect = () => {
+  const { isAuthenticated, user, isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isAuthenticated && user) {
+    const username = user.nickname || user.name;
+    return <Navigate to={`/${username}`} replace />;
+  }
+  return <LoggedOut />;
+};
+
   return (
-    <Auth0Provider 
+    <Auth0Provider
       domain={domain}
       clientId={clientId}
       authorizationParams={{
-        redirect_uri: window.location.origin
-      }}>
-    <ViewContext value={{ viewMode, setViewMode }}>
-      <Router>
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <ViewContext.Provider value={{ viewMode, setViewMode }}>
+        <Router>
           <Routes>
+            <Route path="/" element={<HomeRedirect />} />
             <Route
-              path="/"
+              path="/:username"
               element={
                 <div className="App">
                   <LightBallsOverlay />
@@ -41,7 +58,7 @@ const App = () => {
             <Route path="/loggedOut" element={<LoggedOut />} />
           </Routes>
         </Router>
-    </ViewContext>
+      </ViewContext.Provider>
     </Auth0Provider>
   );
 };
