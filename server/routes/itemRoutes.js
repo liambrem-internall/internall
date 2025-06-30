3cab9da5f82b8142fe08861d6141db43f1971686
Liam Edward Brem <liambrem@meta.com>
2025-06-30 11:17:49 -0700

    api endpoints & schemas

--- server/routes/itemRoutes.js
+++ server/routes/itemRoutes.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const itemController = require('../controllers/itemController');
 
+
+router.get('/:sectionId/items', itemController.getItems);
+
+router.post('/:sectionId/items', itemController.createItem);
+
+router.put('/:sectionId/items/:itemId', itemController.updateItem);
+
+router.delete('/:sectionId/items/:itemId', itemController.deleteItem);
 
 module.exports = router;
