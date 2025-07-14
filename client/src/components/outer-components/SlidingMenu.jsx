e01b64c3cfe5073b8bade5eeb54bc6d3b4f86de4
Liam Edward Brem <liambrem@meta.com>
2025-07-14 10:27:40 -0700

    placeholder

--- client/src/components/outer-components/SlidingMenu.jsx
+++ client/src/components/outer-components/SlidingMenu.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./SlidingMenu.css";
 import Searchbar from "./Searchbar";
 import { IoMdClose } from "react-icons/io";
+import { FaSearch } from "react-icons/fa";
 
 
 const SlidingMenu = ({ open, onClose }) => {
@@ -14,7 +15,11 @@ const SlidingMenu = ({ open, onClose }) => {
         </button>
       </div>
       <Searchbar />
-      {/* Add search results here later */}
+      {/* search results will go here */}
+      <div className="search-placeholder">
+        <FaSearch size={100} />
+        <p>Start typing to see results...</p>
+      </div>
     </div>
   );
 };
