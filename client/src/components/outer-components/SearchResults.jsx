5a8bda43e6c0205b83a66950f9453e2fc3f3fa7f
liambrem <liam.e.brem@gmail.com>
2025-07-17 11:28:05 -0700

    sorting by scores

--- client/src/components/outer-components/SearchResults.jsx
+++ client/src/components/outer-components/SearchResults.jsx
@@ -1,23 +1,35 @@
 import "./SearchResults.css";
 import ItemResults from "./result-types/ItemResults";
-import SectionResults from "./result-types/SectionResults";
 import WebResults from "./result-types/WebResults";
 
-const SearchResults = ({
-  items = [],
-  webResults = {},
-  semantic = {},
-  onItemClick,
-  onDdgClick,
-}) => {
+const SearchResults = ({ results = [], onItemClick, onDdgClick }) => {
+  if (!results.length) return null;
+
   return (
     <div className="search-results">
-      <h3>Fuzzy Items</h3>
-      <ItemResults items={items} onItemClick={onItemClick} />
-      <h3>Semantic Items</h3>
-      <ItemResults items={semantic.items || []} onItemClick={onItemClick} />
-      <h3>Web Results</h3>
-      <WebResults webResults={webResults} onDdgClick={onDdgClick}/>
+      <ul>
+        {results.map((result, idx) => {
+          if (result.type === "item") {
+            return (
+              <ItemResults
+                key={result.data._id}
+                items={[result.data]}
+                onItemClick={onItemClick}
+              />
+            );
+          }
+          if (result.type === "web") {
+            return (
+              <WebResults
+                key={result.data.FirstURL || idx}
+                webResults={{ RelatedTopics: [result.data] }}
+                onDdgClick={onDdgClick}
+              />
+            );
+          }
+          return null;
+        })}
+      </ul>
     </div>
   );
 };
