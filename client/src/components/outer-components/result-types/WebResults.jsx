87165acb4bbb34c99a5887db090afac8193c8aa8
liambrem <liam.e.brem@gmail.com>
2025-07-17 12:29:17 -0700

    code separation

--- client/src/components/outer-components/result-types/WebResults.jsx
+++ client/src/components/outer-components/result-types/WebResults.jsx
@@ -22,6 +22,7 @@ const WebResults = ({ webResults, onDdgClick }) => {
                   Learn more
                 </a>
               )}
+              
             </li>
           ))}
       </ul>
