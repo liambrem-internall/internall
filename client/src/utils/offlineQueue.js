a3d5a734a7f60d77377b58560c648fc25484a57c
liambrem <liam.e.brem@gmail.com>
2025-07-21 14:16:23 -0700

    formatting

--- client/src/utils/offlineQueue.js
+++ client/src/utils/offlineQueue.js
@@ -1,13 +1,15 @@
+const PENDING_EDITS = "pendingEdits";
+
 export function addPendingEdit(edit) {
-  const queue = JSON.parse(localStorage.getItem("pendingEdits") || "[]");
+  const queue = JSON.parse(localStorage.getItem(PENDING_EDITS) || "[]");
   queue.push(edit);
-  localStorage.setItem("pendingEdits", JSON.stringify(queue));
+  localStorage.setItem(PENDING_EDITS, JSON.stringify(queue));
 }
 
 export function getPendingEdits() {
-  return JSON.parse(localStorage.getItem("pendingEdits") || "[]");
+  return JSON.parse(localStorage.getItem(PENDING_EDITS) || "[]");
 }
 
 export function clearPendingEdits() {
-  localStorage.removeItem("pendingEdits");
+  localStorage.removeItem(PENDING_EDITS);
 }
