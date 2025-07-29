b53909fc74da05e1649d11c25f0505d43d0f25fc
liambrem <liam.e.brem@gmail.com>
2025-07-29 09:41:37 -0700

    edit item button

--- client/src/components/main-content/Items/SortableItem.jsx
+++ client/src/components/main-content/Items/SortableItem.jsx
@@ -4,14 +4,19 @@ import { BsGripVertical } from "react-icons/bs";
 import { DraggableComponentTypes } from "../../../utils/constants";
 import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import "./SortableItem.css";
+import { FaEdit } from "react-icons/fa";
+import { useState } from "react";
 
 const SortableItem = ({
   id,
   content,
   sectionId,
+  link,
+  onEdit,
   editingUsers = {},
   users = [],
 }) => {
+  const [hovered, setHovered] = useState(false);
   const {
     setNodeRef,
     attributes,
@@ -23,7 +28,6 @@ const SortableItem = ({
     id,
     data: { type: DraggableComponentTypes.ITEM, sectionId },
   });
-
 
   const editingUserEntry = Object.entries(editingUsers).find(
     ([, value]) => value.itemId === id
@@ -44,30 +48,58 @@ const SortableItem = ({
     "--border-color": borderColor,
   };
 
+  const handleItemClick = (e) => {
+    // Only open link if not clicking edit or drag handle
+    if (link) {
+      window.open(link, "_blank", "noopener,noreferrer");
+    }
+  };
+
   return (
     <div
       className={`sortable-item${editingUserEntry ? " editing" : ""}`}
       ref={setNodeRef}
       style={style}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      onClick={handleItemClick}
     >
       {content}
-      <span
-        className="drag-handle"
-        {...attributes}
-        {...listeners}
-        onClick={(e) => e.stopPropagation()}
-        style={{ cursor: "grab", marginLeft: 8 }}
-      >
-        <OverlayTrigger
-          placement="top"
-          delay={{ show: 800, hide: 100 }}
-          overlay={
-            <Tooltip id="reorder-section-tooltip">Reorder Items</Tooltip>
-          }
+      <div className="right-controls">
+        <span
+          className="edit-handle"
+          onClick={(e) => {
+            e.stopPropagation();
+            onEdit();
+          }}
+          style={{ pointerEvents: hovered ? "auto" : "none" }}
         >
-          <BsGripVertical size={20} color="white" />
-        </OverlayTrigger>
-      </span>
+          <OverlayTrigger
+            placement="top"
+            delay={{ show: 400, hide: 100 }}
+            overlay={<Tooltip id="edit-item-tooltip">Edit Item</Tooltip>}
+          >
+            <FaEdit size={18} color="white" />
+          </OverlayTrigger>
+        </span>
+        <span
+          className="drag-handle"
+          {...attributes}
+          {...listeners}
+          onClick={(e) => e.stopPropagation()}
+          style={{ cursor: "grab", marginLeft: 8 }}
+        >
+          <OverlayTrigger
+            placement="top"
+            delay={{ show: 800, hide: 100 }}
+            overlay={
+              <Tooltip id="reorder-section-tooltip">Reorder Items</Tooltip>
+            }
+          >
+            <BsGripVertical size={20} color="white" />
+          </OverlayTrigger>
+        </span>
+      </div>
     </div>
   );
 };
