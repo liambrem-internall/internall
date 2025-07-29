5daf079524d95878e76a5f05617ce3de8f159e89
liambrem <liam.e.brem@gmail.com>
2025-07-29 09:15:26 -0700

    section modal styles

--- client/src/components/main-content/Sections/SectionModal.jsx
+++ client/src/components/main-content/Sections/SectionModal.jsx
@@ -8,24 +8,26 @@ const SectionModal = ({
   setPendingSectionTitle,
   handleSaveSection,
 }) => {
-
-
   return (
     <Modal show={show} onHide={onHide}>
       <Modal.Header closeButton>
-        <Modal.Title>Enter Section Title Content</Modal.Title>
+        <Modal.Title>Add Section</Modal.Title>
       </Modal.Header>
-      <Form.Control
-        id="sectionTitle"
-        size="lg"
-        type="text"
-        placeholder="Section Title"
-        value={pendingSectionTitle}
-        onChange={(e) => setPendingSectionTitle(e.target.value)}
-        required
-      />
+      <Form.Group className="mb-3" controlId="sectionTitle">
+        <Form.Control
+          type="text"
+          placeholder="Section Title"
+          className="form-control"
+          value={pendingSectionTitle}
+          onChange={(e) => setPendingSectionTitle(e.target.value)}
+        />
+      </Form.Group>
       <Modal.Footer>
-        <Button variant="primary" onClick={handleSaveSection} disabled={!pendingSectionTitle.trim()}>
+        <Button
+          variant="primary"
+          onClick={handleSaveSection}
+          disabled={!pendingSectionTitle.trim()}
+        >
           Add Section
         </Button>
       </Modal.Footer>
